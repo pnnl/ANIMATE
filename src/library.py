4e6e5d8168bc1f7e5014df6a48c63f1d8d5ecf84
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2021-09-17 02:31:19 -0700

    modify cases and demo case plot options

--- src/library.py
+++ src/library.py
@@ -26,11 +26,7 @@ class IntegratedEconomizerControl(CheckLibBase):
 
     def check_detail(self):
         print("Verification results dict: ")
-        output = {
-            "Sample #": len(self.result),
-            "Pass #": len(self.result[self.result == True]),
-            "Fail #": len(self.result[self.result == False])
-        }
+        output = {"Sample #": len(self.result), "Verification Passed?": self.check_bool()}
         print(output)
 
 
@@ -44,12 +40,14 @@ class SupplyAirTempReset(RuleCheckBase):
 
         self.result = (t_sa_set_max - t_sa_set_min) >= (
             self.df["T_z_coo"] - t_sa_set_min
-        ) * 0.25 * 0.99 # 0.99 being the numeric threshold
+        ) * 0.25 * 0.99  # 0.99 being the numeric threshold
 
     def plot(self, plot_option, plt_pts=None):
-        print("Specific plot method implemented, additional distribution plot is being added!")
+        print(
+            "Specific plot method implemented, additional distribution plot is being added!"
+        )
         # plt.hist(self.df['T_sa_set'], bins=10)
-        sns.distplot(self.df['T_sa_set'])
+        sns.distplot(self.df["T_sa_set"])
         plt.title("All samples distribution of T_sa_set")
         plt.show()
 
@@ -61,9 +59,10 @@ class SupplyAirTempReset(RuleCheckBase):
             daystr = f"{str(one_day.year)}-{str(one_day.month)}-{str(one_day.day)}"
             daydf = self.df[daystr]
             day = self.result[daystr]
-            if daydf['T_sa_set'].max() - daydf['T_sa_set'].min() > 0:
+            if daydf["T_sa_set"].max() - daydf["T_sa_set"].min() > 0:
                 return day, daydf
             return day, daydf
+
 
 class EconomizerHighLimitA(RuleCheckBase):
     points = ["oa_db", "oa_threshold", "oa_min_flow", "oa_flow"]
@@ -145,14 +144,16 @@ class HWReset(RuleCheckBase):
 
     def verify(self):
         self.result = (
-            (self.df["m_hw"] <= 0)
+            (
+                self.df["m_hw"] <= 0
+            )  # add boundary relaxation in the rules for this one and chwreset
             | (
                 (self.df["T_oa_db"] <= self.df["T_oa_min"])
-                & (self.df["T_hw"] >= self.df["T_hw_max_st"])
+                & (self.df["T_hw"] >= self.df["T_hw_max_st"] * 0.99)
             )
             | (
-                (self.df["T_oa_db"] >= self.df["T_oa_max"])
-                & (self.df["T_hw"] <= self.df["T_hw_min_st"])
+                (self.df["T_oa_db"] >= (self.df["T_oa_max"]))
+                & (self.df["T_hw"] <= self.df["T_hw_min_st"] * 1.01)
             )
             | (
                 (
@@ -160,11 +161,22 @@ class HWReset(RuleCheckBase):
                     & (self.df["T_oa_db"] <= self.df["T_oa_max"])
                 )
                 & (
-                    (self.df["T_hw"] >= self.df["T_hw_min_st"])
-                    & (self.df["T_hw"] <= self.df["T_hw_max_st"])
+                    (self.df["T_hw"] >= self.df["T_hw_min_st"] * 0.99)
+                    & (self.df["T_hw"] <= self.df["T_hw_max_st"] * 1.01)
                 )
             )
         )
+
+    # Add a correlation scatter plot of T_oa_db and T_hw
+    def plot(self, plot_option, plt_pts=None):
+        print(
+            "Specific plot method implemented, additional scatter plot is being added!"
+        )
+        sns.scatterplot(x="T_oa_db", y="T_hw", data=self.df)
+        plt.title("Scatter plot between T_oa_db and T_hw")
+        plt.show()
+
+        super().plot(plot_option, plt_pts)
 
 
 class CHWReset(RuleCheckBase):
@@ -183,11 +195,11 @@ class CHWReset(RuleCheckBase):
             (self.df["m_chw"] <= 0)
             | (
                 (self.df["T_oa_db"] <= self.df["T_oa_min"])
-                & (self.df["T_chw"] >= self.df["T_chw_max_st"])
+                & (self.df["T_chw"] >= self.df["T_chw_max_st"] * 0.99)
             )
             | (
                 (self.df["T_oa_db"] >= self.df["T_oa_max"])
-                & (self.df["T_chw"] <= self.df["T_chw_min_st"])
+                & (self.df["T_chw"] <= self.df["T_chw_min_st"] * 1.01)
             )
             | (
                 (
@@ -195,17 +207,28 @@ class CHWReset(RuleCheckBase):
                     & (self.df["T_oa_db"] <= self.df["T_oa_max"])
                 )
                 & (
-                    (self.df["T_chw"] >= self.df["T_chw_min_st"])
-                    & (self.df["T_chw"] <= self.df["T_chw_max_st"])
+                    (self.df["T_chw"] >= self.df["T_chw_min_st"] * 0.99)
+                    & (self.df["T_chw"] <= self.df["T_chw_max_st"] * 1.01)
                 )
             )
         )
+
+    # Add a correlation scatter plot of T_oa_db and T_chw
+    def plot(self, plot_option, plt_pts=None):
+        print(
+            "Specific plot method implemented, additional scatter plot is being added!"
+        )
+        sns.scatterplot(x="T_oa_db", y="T_chw", data=self.df)
+        plt.title("Scatter plot between T_oa_db and T_chw")
+        plt.show()
+
+        super().plot(plot_option, plt_pts)
 
 class ZoneHeatSetpointMinimum(CheckLibBase):
     points = ["T_heat_set"]
 
     def verify(self):
-        self.result = (self.df["T_heat_set"] <= 12.78)
+        self.result = self.df["T_heat_set"] <= 12.78
 
     def check_bool(self) -> bool:
         if len(self.result[self.result == True] > 0):
@@ -218,15 +241,16 @@ class ZoneHeatSetpointMinimum(CheckLibBase):
         output = {
             "Sample #": len(self.result),
             "Pass #": len(self.result[self.result == True]),
-            "Fail #": len(self.result[self.result == False])
+            "Fail #": len(self.result[self.result == False]),
         }
         print(output)
+
 
 class ZoneCoolingSetpointMaximum(CheckLibBase):
     points = ["T_cool_set"]
 
     def verify(self):
-        self.result = (self.df["T_cool_set"] >= 32.22)
+        self.result = self.df["T_cool_set"] >= 32.22
 
     def check_bool(self) -> bool:
         if len(self.result[self.result == True] > 0):
@@ -239,9 +263,10 @@ class ZoneCoolingSetpointMaximum(CheckLibBase):
         output = {
             "Sample #": len(self.result),
             "Pass #": len(self.result[self.result == True]),
-            "Fail #": len(self.result[self.result == False])
+            "Fail #": len(self.result[self.result == False]),
         }
         print(output)
+
 
 class ZoneHeatingResetDepth(CheckLibBase):
     points = ["T_heat_set"]
@@ -249,7 +274,6 @@ class ZoneHeatingResetDepth(CheckLibBase):
     def verify(self):
         self.t_heat_set_min = min(sef.df["T_heat_set"])
         self.t_heat_set_max = max(sef.df["T_heat_set"])
-
 
         self.result = (self.t_heat_set_max - self.t_heat_set_min) >= 5.55
 
@@ -261,9 +285,10 @@ class ZoneHeatingResetDepth(CheckLibBase):
         output = {
             "Sample #": len(self.result),
             "max(T_heat_set)": self.t_heat_set_max,
-            "min(T_heat_set": self.t_heat_set_min
+            "min(T_heat_set": self.t_heat_set_min,
         }
         print(output)
+
 
 class ZonecoolingResetDepth(CheckLibBase):
     points = ["T_cool_set"]
@@ -271,7 +296,6 @@ class ZonecoolingResetDepth(CheckLibBase):
     def verify(self):
         self.t_cool_set_min = min(sef.df["T_cool_set"])
         self.t_cool_set_max = max(sef.df["T_cool_set"])
-
 
         self.result = (self.t_cool_set_max - self.t_cool_set_min) >= 2.77
 
@@ -283,6 +307,6 @@ class ZonecoolingResetDepth(CheckLibBase):
         output = {
             "Sample #": len(self.result),
             "max(T_cool_set)": self.t_cool_set_max,
-            "min(T_cool_set": self.t_cool_set_min
+            "min(T_cool_set": self.t_cool_set_min,
         }
         print(output)
