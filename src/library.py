92ae703a273c18416754bcc6620635ec9149643b
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2022-03-30 16:06:42 -0700

    minor clean up

--- src/library.py
+++ src/library.py
@@ -354,8 +354,7 @@ class NightCycleOperation(CheckLibBase):
     ]
 
     def verify(self):
-        self.df["night_cycle_observed"] = "NA"
-        # self.df = self.df.reset_index()
+        self.df["night_cycle_observed"] = np.nan
         for index, row in self.df.iterrows():
             if self.df["HVAC_operation_sch"][index] == 0:
                 if self.df["Fan_elec_rate"][index] == 0:
@@ -376,41 +375,8 @@ class NightCycleOperation(CheckLibBase):
                         self.df.loc[index, "night_cycle_observed"] = 1
                     else:
                         self.df.loc[index, "night_cycle_observed"] = 1
-            else:
-                self.df.loc[index, "night_cycle_observed"] = np.nan
 
-        self.result = (self.df["night_cycle_observed"] == 1)
-
-    def plot(self, plot_option, plt_pts=None):
-        print("Specific plot method implemented, additional line plot is being added!")
-        fig, (ax1, ax2, ax3) = plt.subplots(
-            1,
-            3,
-        )
-        fig.suptitle(
-            "Night Cycle Operation Control Verification - Last week of the year"
-        )
-
-        sns.lineplot(data=self.df["T_zone"][8592:8760], ax=ax1)
-        sns.lineplot(data=self.df["T_heat_set"][8592:8760], ax=ax1)
-        sns.lineplot(data=self.df["T_cool_set"][8592:8760], ax=ax1)
-        ax1.set_title("Temperature [°C]")
-        ax1.set_xlabel("Time Step")
-        ax1.set_ylabel("Temperature")
-
-        sns.lineplot(data=self.df["Fan_elec_rate"][8592:8760], ax=ax2)
-        ax2.set_title("Fan Electricity Rate [-]")
-        ax2.set_xlabel("Time Step")
-        ax2.set_ylabel("Power")
-
-        sns.lineplot(data=self.df["HVAC_operation_sch"][8592:8760], ax=ax3)
-        ax3.set_title("HVAC operation schedule")
-        ax3.set_xlabel("Time Step")
-        ax3.set_ylabel("Ratio [-]")
-
-        plt.show()
-
-        super().plot(plot_option, plt_pts)
+        self.result = self.df["night_cycle_observed"] == 1
 
     def check_bool(self) -> bool:
         return True  # TODO should be updated later.
