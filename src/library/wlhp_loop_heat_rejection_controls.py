71a04fc8abfbd5ecfccd8669eed5e720546a8301
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2023-01-09 09:37:44 -0800

    Paint it Black!

--- src/library/wlhp_loop_heat_rejection_controls.py
+++ src/library/wlhp_loop_heat_rejection_controls.py
@@ -5,13 +5,9 @@ class WLHPLoopHeatRejectionControl(RuleCheckBase):
     points = ["T_loop", "m_pump", "tol"]
 
     def verify(self):
-        self.df["T_max_loop"] = (
-            self.df.query("m_pump >0")["T_loop"]
-        ).max()
-        self.df["T_min_min"] = (
-            self.df.query("m_pump >0")["T_loop"]
-        ).min()
+        self.df["T_max_loop"] = (self.df.query("m_pump >0")["T_loop"]).max()
+        self.df["T_min_min"] = (self.df.query("m_pump >0")["T_loop"]).min()
 
-        self.result = (
-            self.df["T_max_loop"] - self.df["T_min_min"]
-        ) > 11.11 - self.df["tol"]
+        self.result = (self.df["T_max_loop"] - self.df["T_min_min"]) > 11.11 - self.df[
+            "tol"
+        ]
