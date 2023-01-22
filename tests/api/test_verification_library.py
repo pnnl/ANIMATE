2adfa5b00872f70e01f955d7d569f7eb73eccc28
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2023-01-21 17:59:00 -0800

    add getter tests

--- tests/api/test_verification_library.py
+++ tests/api/test_verification_library.py
@@ -34,6 +34,25 @@ class TestVerificationLibrary(unittest.TestCase):
         self.assertEqual(len(vl_obj.lib_items_json_path), len(exp_items))
         self.assertEqual(len(vl_obj.lib_items_python_path), len(exp_items))
 
+    def test_get_library_item(self):
+        vl_obj = VerificationLibrary(lib_path)
+
+        item = vl_obj.get_library_item("AutomaticShutdown")
+        self.assertEqual(item["library_item_name"], "AutomaticShutdown")
+        self.assertTrue(isinstance(item["library_definition"], dict))
+        self.assertEqual(item["library_python_path"].split(".")[-1], "py")
+        self.assertEqual(item["library_json_path"].split(".")[-1], "json")
+
+    def test_get_library_item_invalid(self):
+        vl_obj = VerificationLibrary(lib_path)
+
+        with self.assertLogs("root", level="ERROR") as logobs:
+            item = vl_obj.get_library_item("NonExistLibItem")
+            self.assertEqual(
+                "ERROR:root:NonExistLibItem is not in loaded library items.",
+                logobs.output[0],
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
