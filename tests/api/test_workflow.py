e08b8d8303d8c08bcd6f42b800b10bf9791b0511
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2023-03-31 14:32:30 -0700

    update workflow demo

--- tests/api/test_workflow.py
+++ tests/api/test_workflow.py
@@ -1,3 +1,5 @@
+import warnings
+
 import unittest, sys, datetime, copy
 
 sys.path.append("./src")
@@ -7,6 +9,8 @@ from api import Workflow
 
 class TestWorkflow(unittest.TestCase):
     def test_run_workflow(self):
+        warnings.simplefilter(action="ignore", category=FutureWarning)
+        warnings.simplefilter(action="ignore", category=ResourceWarning)
         workflow = Workflow(workflow="./demo/api_demo/demo_workflow.json")
         workflow.run_workflow(verbose=True)
 
